7ca79add448238006d47812a2dd57ebdc0440862
Yuta OGIHARA <ogi@zsrv.net>
2014-01-09 16:02:01 +0900

    Initial Commit

---
+++ FTTDDTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FTTDDTest
+//
+//  Created by Yuta OGIHARA on 1/9/14.
+//  Copyright (c) 2014 Yuta OGIHARA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FTTDDTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FTTDDTest
+//
+//  Created by Yuta OGIHARA on 1/9/14.
+//  Copyright (c) 2014 Yuta OGIHARA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
